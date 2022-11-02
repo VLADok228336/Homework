f2a657cfe5bcb92feb16bede5b0eef6878ed8acb
VLADok228336 <chertkov_vlad@rambler.ru>
2022-11-02 17:35:34 +0300

    Добавил функцию добавления двухклеточных кораблей

--- SeaWar/tkiinter.py
+++ SeaWar/tkiinter.py
@@ -25,4 +25,33 @@ for i in range(4):
     c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
     corsx[i] = corx
     corsy[i] = cory
+for i in range(3):
+    corx = randint(1, 10)
+    cory = randint(1, 10)
+    if(i != 0):
+        if(corsx.__contains__(corx) and corsy.__contains__(cory)):
+            i -= 1
+            continue
+    c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
+    corx+=1
+    if(corsx.__contains__(corx) and corsy.__contains__(cory) or corsx.__contains__(corx-1) and corsy.__contains__(cory) or corsx.__contains__(corx+1) and corsy.__contains__(cory) or corsx.__contains__(corx) and corsy.__contains__(cory+1) or corsx.__contains__(corx) and corsy.__contains__(cory -1)):
+        corx-=2
+        if(corsx.__contains__(corx) and corsy.__contains__(cory) or corsx.__contains__(corx-1) and corsy.__contains__(cory) or corsx.__contains__(corx+1) and corsy.__contains__(cory) or corsx.__contains__(corx) and corsy.__contains__(cory+1) or corsx.__contains__(corx) and corsy.__contains__(cory -1)):
+            corx+=1
+            cory += 1
+            if(corsx.__contains__(corx) and corsy.__contains__(cory) or corsx.__contains__(corx-1) and corsy.__contains__(cory) or corsx.__contains__(corx+1) and corsy.__contains__(cory) or corsx.__contains__(corx) and corsy.__contains__(cory+1) or corsx.__contains__(corx) and corsy.__contains__(cory -1)):
+                cory-=2
+                if(corsx.__contains__(corx) and corsy.__contains__(cory) or corsx.__contains__(corx-1) and corsy.__contains__(cory) or corsx.__contains__(corx+1) and corsy.__contains__(cory) or corsx.__contains__(corx) and corsy.__contains__(cory+1) or corsx.__contains__(corx) and corsy.__contains__(cory -1)):
+                    i -= 1
+                    continue
+                else:
+                    c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
+            else:
+                c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
+        else:
+            c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
+    else:
+        c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
+        corsx[i+4] = corx
+        corsy[i+4] = cory
 root.mainloop()
