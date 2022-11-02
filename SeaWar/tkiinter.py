c19f2c29dec6c89a536af41cbbd6fedc2cf5a387
VLADok228336 <chertkov_vlad@rambler.ru>
2022-11-02 17:05:40 +0300

    Исправил баг с квадратами

--- SeaWar/tkiinter.py
+++ SeaWar/tkiinter.py
@@ -2,7 +2,13 @@ from tkinter import*
 from random import*
 root = Tk()
 root.title("War at the Sea")
-c = Canvas(root, width=510, height=510, background="white")
+
+fieldSize = 500
+
+
+c = Canvas(root, width=fieldSize+10, height=fieldSize+10, background="white")
+cellSize = 50
+
 c.pack()
 for i in range(5, 555, 50):
     c.create_line(i, 5, i, 505)
@@ -11,5 +17,5 @@ for i in range(5, 555, 50):
 for i in range(10):
     corx = randint(1, 10)
     cory = randint(1, 10)
-    c.create_rectangle(corx * 50 +5, cory * 50 +5, corx* 51 -45, cory * 51 - 45, fill="red")
+    c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
 root.mainloop()
