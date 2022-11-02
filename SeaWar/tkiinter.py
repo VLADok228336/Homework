e1874415ea3fb6627ca7e5b618d0a9cf2a4af63d
VLADok228336 <chertkov_vlad@rambler.ru>
2022-11-02 18:10:33 +0300

    отменил функцию создания 3-х клеточных кораблей Абоба

--- SeaWar/tkiinter.py
+++ SeaWar/tkiinter.py
@@ -15,43 +15,44 @@ for i in range(5, 555, 50):
     c.create_line(i, 5, i, 505)
 for i in range(5, 555, 50):
     c.create_line(5, i, 505, i)
-for i in range(4):
-    corx = randint(1, 10)
-    cory = randint(1, 10)
-    if(i != 0):
+def ships():
+    for i in range(4):
+        corx = randint(1, 10)
+        cory = randint(1, 10)
+        if(i != 0):
+            if(corsx.__contains__(corx) and corsy.__contains__(cory)):
+                i -= 1
+                continue
+        c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
+        corsx[i] = corx
+        corsy[i] = cory
+    for i in range(3):
+        corx = randint(1, 10)
+        cory = randint(1, 10)
         if(corsx.__contains__(corx) and corsy.__contains__(cory)):
             i -= 1
             continue
-    c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
-    corsx[i] = corx
-    corsy[i] = cory
-for i in range(3):
-    corx = randint(1, 10)
-    cory = randint(1, 10)
-    if(i != 0):
-        if(corsx.__contains__(corx) and corsy.__contains__(cory)):
-            i -= 1
-            continue
-    c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
-    corx+=1
-    if(corsx.__contains__(corx) and corsy.__contains__(cory) or corsx.__contains__(corx-1) and corsy.__contains__(cory) or corsx.__contains__(corx+1) and corsy.__contains__(cory) or corsx.__contains__(corx) and corsy.__contains__(cory+1) or corsx.__contains__(corx) and corsy.__contains__(cory -1)):
-        corx-=2
-        if(corsx.__contains__(corx) and corsy.__contains__(cory) or corsx.__contains__(corx-1) and corsy.__contains__(cory) or corsx.__contains__(corx+1) and corsy.__contains__(cory) or corsx.__contains__(corx) and corsy.__contains__(cory+1) or corsx.__contains__(corx) and corsy.__contains__(cory -1)):
-            corx+=1
-            cory += 1
-            if(corsx.__contains__(corx) and corsy.__contains__(cory) or corsx.__contains__(corx-1) and corsy.__contains__(cory) or corsx.__contains__(corx+1) and corsy.__contains__(cory) or corsx.__contains__(corx) and corsy.__contains__(cory+1) or corsx.__contains__(corx) and corsy.__contains__(cory -1)):
-                cory-=2
-                if(corsx.__contains__(corx) and corsy.__contains__(cory) or corsx.__contains__(corx-1) and corsy.__contains__(cory) or corsx.__contains__(corx+1) and corsy.__contains__(cory) or corsx.__contains__(corx) and corsy.__contains__(cory+1) or corsx.__contains__(corx) and corsy.__contains__(cory -1)):
-                    i -= 1
-                    continue
+        c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
+        corx+=1
+        if(corx>10 or cory>10 or corx<1 or cory < 1 or corsx.__contains__(corx) and corsy.__contains__(cory) or corsx.__contains__(corx-1) and corsy.__contains__(cory) or corsx.__contains__(corx+1) and corsy.__contains__(cory) or corsx.__contains__(corx) and corsy.__contains__(cory+1) or corsx.__contains__(corx) and corsy.__contains__(cory -1) or corsx.__contains__(corx+1) and corsy.__contains__(cory +1) or corsx.__contains__(corx+1) and corsy.__contains__(cory -1) or corsx.__contains__(corx-1) and corsy.__contains__(cory +1) or corsx.__contains__(corx-1) and corsy.__contains__(cory -1)):
+            corx-=2
+            if(corx>10 or cory>10 or corx<1 or cory < 1 or corsx.__contains__(corx) and corsy.__contains__(cory) or corsx.__contains__(corx-1) and corsy.__contains__(cory) or corsx.__contains__(corx+1) and corsy.__contains__(cory) or corsx.__contains__(corx) and corsy.__contains__(cory+1) or corsx.__contains__(corx) and corsy.__contains__(cory -1) or  corsx.__contains__(corx+1) and corsy.__contains__(cory +1) or corsx.__contains__(corx+1) and corsy.__contains__(cory -1) or corsx.__contains__(corx-1) and corsy.__contains__(cory +1) or corsx.__contains__(corx-1) and corsy.__contains__(cory -1)):
+                corx+=1
+                cory += 1
+                if(corx>10 or cory>10 or corx<1 or cory < 1 or corsx.__contains__(corx) and corsy.__contains__(cory) or corsx.__contains__(corx-1) and corsy.__contains__(cory) or corsx.__contains__(corx+1) and corsy.__contains__(cory) or corsx.__contains__(corx) and corsy.__contains__(cory+1) or corsx.__contains__(corx) and corsy.__contains__(cory -1) or  corsx.__contains__(corx+1) and corsy.__contains__(cory +1) or corsx.__contains__(corx+1) and corsy.__contains__(cory -1) or corsx.__contains__(corx-1) and corsy.__contains__(cory +1) or corsx.__contains__(corx-1) and corsy.__contains__(cory -1)):
+                    cory-=2
+                    if(corx>10 or cory>10 or corx<1 or cory < 1 or corsx.__contains__(corx) and corsy.__contains__(cory) or corsx.__contains__(corx-1) and corsy.__contains__(cory) or corsx.__contains__(corx+1) and corsy.__contains__(cory) or corsx.__contains__(corx) and corsy.__contains__(cory+1) or corsx.__contains__(corx) and corsy.__contains__(cory -1)or  corsx.__contains__(corx+1) and corsy.__contains__(cory +1) or corsx.__contains__(corx+1) and corsy.__contains__(cory -1) or corsx.__contains__(corx-1) and corsy.__contains__(cory +1) or corsx.__contains__(corx-1) and corsy.__contains__(cory -1)):
+                        i -= 1
+                        continue
+                    else:
+                        c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
                 else:
                     c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
             else:
                 c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
         else:
             c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
-    else:
-        c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
-        corsx[i+4] = corx
-        corsy[i+4] = cory
+            corsx[i+4] = corx
+            corsy[i+4] = cory
+ships()
 root.mainloop()
