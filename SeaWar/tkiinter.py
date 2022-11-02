547ff56c0fcfb5a485f2506c0048aba4376ef83b
VLADok228336 <chertkov_vlad@rambler.ru>
2022-11-02 16:30:18 +0300

    Ничего не сделал, просто коммитнул

--- SeaWar/tkiinter.py
+++ SeaWar/tkiinter.py
@@ -1,6 +1,9 @@
 from tkinter import*
 root = Tk()
 root.title("War at the Sea")
-c = Canvas(root, width=500, height=500, background="white",)
+c = Canvas(root, width=510, height=510, background="white")
 c.pack()
+for i in range(5, 555, 50):
+    c.create_line(i, 5, i, 505)
+
 root.mainloop()
