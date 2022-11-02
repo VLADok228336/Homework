4cd733046d94c33291f7895d2f2a4b25bcf88d24
VLADok228336 <chertkov_vlad@rambler.ru>
2022-11-02 17:11:55 +0300

    Добавил проверку на повторение

--- SeaWar/tkiinter.py
+++ SeaWar/tkiinter.py
@@ -2,7 +2,8 @@ from tkinter import*
 from random import*
 root = Tk()
 root.title("War at the Sea")
-
+corsx = {}
+corsy = {}
 fieldSize = 500
 
 
@@ -14,8 +15,14 @@ for i in range(5, 555, 50):
     c.create_line(i, 5, i, 505)
 for i in range(5, 555, 50):
     c.create_line(5, i, 505, i)
-for i in range(10):
+for i in range(4):
     corx = randint(1, 10)
     cory = randint(1, 10)
+    if(i != 0):
+        if(corsx.__contains__(corx) and corsy.__contains__(cory)):
+            i -= 1
+            continue
     c.create_rectangle(((corx - 1) * 50) +5, ((cory - 1)  * 50) +5, ((corx - 1) * 50) +55, ((cory - 1)  * 50) + 55, fill="red")
+    corsx[i] = corx
+    corsy[i] = cory
 root.mainloop()
